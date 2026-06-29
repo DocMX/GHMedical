b408963c54e94d06a19a22f3bc72985d3cfaa225
DocMX <jorge_cruzer@hotmail.com>
2026-06-28 19:22:09 -0600

    cmabiar de src schemes a src features schemes, agrega pretier

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,15 +1,11 @@
 export const siteConfig = {
   name: "GH Medical Trauma",
-
   description: "Medical Products Management System",
-
   company: "GH Medical Trauma",
-
   version: "1.0.0",
-
   logo: "/logo.jpg",
-
   defaultLanguage: "en",
-
   supportEmail: "ghmedicaltrauma@hotmail.com",
+  locale: "en",
+  currency: "MXN",
 } as const;
